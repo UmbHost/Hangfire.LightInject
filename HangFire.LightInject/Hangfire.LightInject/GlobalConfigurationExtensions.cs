a0a143a69a8a4d3d38d4da93586d194021bb3929
Aaron Sadler <aaron@aaronsadler.uk>
2021-06-10 18:35:22 +0100

    Umbraco Hack for DI support

--- HangFire.LightInject/Hangfire.LightInject/GlobalConfigurationExtensions.cs
+++ HangFire.LightInject/Hangfire.LightInject/GlobalConfigurationExtensions.cs
@@ -1,11 +1,6 @@
 ﻿using Hangfire.Annotations;
-using Hangfire.LightInject;
 using LightInject;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Hangfire
 {
--- HangFire.LightInject/Hangfire.LightInject/LightInjectJobActivator.cs
+++ HangFire.LightInject/Hangfire.LightInject/LightInjectJobActivator.cs
@@ -1,72 +1,103 @@
 ﻿using LightInject;
 using System;
+using System.Web;
+using System.IO;
 using System.Linq;
+using Hangfire;
 
-namespace Hangfire.LightInject
+public class LightInjectJobActivator : JobActivator
 {
-    public class LightInjectJobActivator : JobActivator
+    private readonly ServiceContainer _container;
+    internal static readonly object LifetimeScopeTag = new object();
+
+    public LightInjectJobActivator(ServiceContainer container, bool selfReferencing = false)
     {
-        private readonly ServiceContainer _container;
-        internal static readonly object LifetimeScopeTag = new object();
+        if (container == null)
+            throw new ArgumentNullException("container");
 
-        public LightInjectJobActivator(ServiceContainer container, bool selfReferencing = false)
-        {
-            if (container == null)
-                throw new ArgumentNullException("container");
-
-            this._container = container;
-
-        }
-
-        public override object ActivateJob(Type jobType)
-        {
-            // this will fail if you do self referencing job queues on a class with an interface:
-            //  BackgroundJob.Enqueue(() => this.SendSms(message)); 
-            var instance = _container.TryGetInstance(jobType);
-
-            // since it fails we can try to get the first interface and request from container
-            if (instance == null && jobType.GetInterfaces().Count() > 0)
-                instance = _container.GetInstance(jobType.GetInterfaces().FirstOrDefault());
-
-            return instance;
-
-        }
-
-        public override JobActivatorScope BeginScope()
-        {
-            return new LightInjecterScope(_container);
-        }
+        this._container = container;
 
     }
 
-    class LightInjecterScope : JobActivatorScope
+    public override object ActivateJob(Type jobType)
     {
-        private readonly ServiceContainer _container;
-        private readonly Scope _scope;
-
-        public LightInjecterScope(ServiceContainer container)
+        // IMPORTANT: HACK to create fake http context for job to allow the LightInject PerWebRequestScopeManager to work correctly when running in background jobs
+        // Umbraco is hardcoded to using MixedLightInjectScopeManagerProvider so its really really hard to get around so this hack is the easiest way to handle this.
+        if (HttpContext.Current == null)
         {
-            _container = container;
-
-            _scope = _container.BeginScope();
+            HttpContext.Current = new HttpContext(new HttpRequest("PerWebRequestScopeManager", "https://localhost/PerWebRequestScopeManager", string.Empty),
+                new HttpResponse(new StringWriter()));
         }
 
-        public override object Resolve(Type jobType)
+        // this will fail if you do self referencing job queues on a class with an interface:
+        //  BackgroundJob.Enqueue(() => this.SendSms(message)); 
+        var instance = _container.TryGetInstance(jobType);
+
+        // since it fails we can try to get the first interface and request from container
+        if (instance == null && jobType.GetInterfaces().Count() > 0)
+            instance = _container.GetInstance(jobType.GetInterfaces().FirstOrDefault());
+
+        return instance;
+
+    }
+
+    public override JobActivatorScope BeginScope()
+    {
+        // IMPORTANT: HACK to create fake http context for job to allow the LightInject PerWebRequestScopeManager to work correctly when running in background jobs
+        // Umbraco is hardcoded to using MixedLightInjectScopeManagerProvider so its really really hard to get around so this hack is the easiest way to handle this.
+        if (HttpContext.Current == null)
         {
-
-            var instance = _container.TryGetInstance(jobType);
-
-            // since it fails we can try to get the first interface and request from container
-            if (instance == null && jobType.GetInterfaces().Count() > 0)
-                instance = _container.GetInstance(jobType.GetInterfaces().FirstOrDefault());
-
-            return instance;
-
+            HttpContext.Current = new HttpContext(new HttpRequest("PerWebRequestScopeManager", "https://localhost/PerWebRequestScopeManager", string.Empty),
+                new HttpResponse(new StringWriter()));
         }
 
-        public override void DisposeScope()
+        return new LightInjecterScope(_container);
+    }
+
+}
+
+class LightInjecterScope : JobActivatorScope
+{
+    private readonly ServiceContainer _container;
+    private readonly Scope _scope;
+
+    public LightInjecterScope(ServiceContainer container)
+    {
+
+        _container = container;
+
+        _scope = _container.BeginScope();
+    }
+
+    public override object Resolve(Type jobType)
+    { // IMPORTANT: HACK to create fake http context for job to allow the LightInject PerWebRequestScopeManager to work correctly when running in background jobs
+        // Umbraco is hardcoded to using MixedLightInjectScopeManagerProvider so its really really hard to get around so this hack is the easiest way to handle this.
+        if (HttpContext.Current == null)
         {
-            _scope?.Dispose();
+            HttpContext.Current = new HttpContext(new HttpRequest("PerWebRequestScopeManager", "https://localhost/PerWebRequestScopeManager", string.Empty),
+                new HttpResponse(new StringWriter()));
         }
+
+        var instance = _container.TryGetInstance(jobType);
+
+        // since it fails we can try to get the first interface and request from container
+        if (instance == null && jobType.GetInterfaces().Count() > 0)
+            instance = _container.GetInstance(jobType.GetInterfaces().FirstOrDefault());
+
+        return instance;
+
+    }
+
+    public override void DisposeScope()
+    {
+        // IMPORTANT: HACK to create fake http context for job to allow the LightInject PerWebRequestScopeManager to work correctly when running in background jobs
+        // Umbraco is hardcoded to using MixedLightInjectScopeManagerProvider so its really really hard to get around so this hack is the easiest way to handle this.
+        if (HttpContext.Current == null)
+        {
+            HttpContext.Current = new HttpContext(new HttpRequest("PerWebRequestScopeManager", "https://localhost/PerWebRequestScopeManager", string.Empty),
+                new HttpResponse(new StringWriter()));
+        }
+
+        _scope?.Dispose();
     }
 }
